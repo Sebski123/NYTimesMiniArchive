0d6f5ac131a844d2a5bfc60381b1c3e196305f10
Sebski123 <sebthomle@gmail.com>
2026-01-23 21:36:27 +0100

    Update puzzle data fetching URL and headers in GetData.py

--- tools/GetData.py
+++ tools/GetData.py
@@ -2,12 +2,13 @@ import datetime
 
 import requests
 
-base_url = "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini/"
+url = "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json"
 
 
 headers = {
     # Add your own cookie here in the format: SIDNY={your cookie}; (you can find this in the network tab of the developer tools)
-    'Cookie': 'SIDNY='
+    'Cookie': 'SIDNY=CBcSMQjy9cWtBhCgko-xBhoSMS1bLMe5bSs5-e_fV_NSlxp6IPH6yEoqAh53ONaVnv8FQgAaQBy4V4Ak8BSxDNwyyBtUi6oMLUVnO0Yhd4unbrojhY2JS6siU1LQAYIRW6y8yN6dRf1fXb_X4CR24Dx1kYZysQA=;',
+    "x-games-auth-bypass": "true",
 }
 
 
@@ -29,7 +30,7 @@ def fetch_puzzle(date: datetime.datetime):
     day = date.day
 
     # generate the url in the format: base_url + 2024-04-01.json
-    url = base_url + f"{year}-{month:02d}-{day:02d}.json"
+    # url = base_url + f"{year}-{month:02d}-{day:02d}.json"
 
     # Fetch the data from the url
     response = requests.request("GET", url, headers=headers)
